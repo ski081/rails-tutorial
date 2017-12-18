474f553dbadb39fed66ca17c454d783a479c851c
Mark Struzinski <mark.struzinski@lowes.com>
2017-12-18 15:32:18 -0500

    Adding login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
-    has_secure_password
-    before_save { self.email = email.downcase }
+  has_secure_password
+  before_save { self.email = email.downcase }
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :email, presence: true, length: { maximum: 255 }, 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, 
                 format: { with: VALID_EMAIL_REGEX }, 
                 uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
